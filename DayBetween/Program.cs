b60e343da6220e236f63887150071442af97f292
sebudwell@gmail.com <sebudwell@gmail.com>
2019-03-10 16:35:14 -0500

    Done

--- DayBetween/Program.cs
+++ DayBetween/Program.cs
@@ -7,10 +7,31 @@ namespace DaysBetween
         public static void Main()
         {
             // TODO:  Get two dates from the user.
+            DateTime firstDate = new DateTime(1988, 5, 4);
+            DateTime secondDate = new DateTime(2019, 3, 10);
+
+            
 
             Console.ReadLine();
         }
 
+        public static int DaysBetween(DateTime firstDate, DateTime secondDate)
+        {
+            
+            if (secondDate > firstDate)
+            {
+                TimeSpan difference = secondDate - firstDate;
+                return difference.Days;
+            }
+            if (secondDate < firstDate)
+            {
+                TimeSpan difference = firstDate - secondDate;
+                return difference.Days;
+            }
+
+            return 0;
+
+        }
         // TODO: Create that has a method that counts the number of days between two different dates.
     }
 }
